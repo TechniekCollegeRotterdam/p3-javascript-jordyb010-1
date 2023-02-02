3244b2c3472b495e104bd5023743054d38b7cf1a
jordyb010 <9015306@student.zadkine.nl>
2023-02-02 09:39:44 +0100

    oefenen in de klass

--- week-1/script.js
+++ week-1/script.js
@@ -1,5 +1,9 @@
-let header = document.getElementById("header");
-header.style.color = "green";
+// let header = document.getElementById("header");
+// header.style.color = "green";
 
-document.getElementById("header");
-header.innerHTML = "Jordy Bakker";
+// document.getElementById("header");
+// header.innerHTML = "Jordy Bakker";
+
+let text = document.getElementById("change");
+text.style.color = "red";
+change .innerHTML = "Jordy Bakker"
