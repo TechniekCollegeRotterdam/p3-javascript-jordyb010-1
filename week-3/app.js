52bf3aa6d3a74c92e0cbe7eedf62de5770916f0c
jordyb010 <9015306@student.zadkine.nl>
2023-02-16 12:13:13 +0100

    green color

--- week-3/app.js
+++ week-3/app.js
@@ -5,6 +5,7 @@ let getId =  document.getElementById("bud");
 if (budget > product){
     getId.innerText = "u heeft genoeg geld"
     // console.log ("u heeft genoeg geld");
+    bud.style.color = "green";
 } else {
     getId.innerText = "u heeft te weinig geld"
     bud.style.color = "red";
