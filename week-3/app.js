5c3b0850b240bb170b185e9fe04ddf727fe46bea
jordyb010 <9015306@student.zadkine.nl>
2023-02-16 12:56:49 +0100

    button click to 10

--- week-3/app.js
+++ week-3/app.js
@@ -1,13 +1,23 @@
-let budget = 100;
-let product = prompt();
-let getId =  document.getElementById("bud");
+// let budget = 100;
+// let product = prompt();
+// let getId =  document.getElementById("bud");
 
-if (budget > product){
-    getId.innerText = "u heeft genoeg geld"
-    // console.log ("u heeft genoeg geld");
-    bud.style.color = "green";
-} else {
-    getId.innerText = "u heeft te weinig geld"
-    bud.style.color = "red";
-    // console.log ("u heeft te weinig geld")
+// if (budget > product){
+//     getId.innerText = "u heeft genoeg geld"
+//     // console.log ("u heeft genoeg geld");
+//     bud.style.color = "green";
+// } else {
+//     getId.innerText = "u heeft te weinig geld"
+//     bud.style.color = "red";
+//     // console.log ("u heeft te weinig geld")
+// }
+let getal = 0;
+
+function verhoogGetal(){
+    getal++;
+    document.getElementById("getal").innerHTML = getal;
+
+    if (getal ===10){
+        getal = 0;
+    }
 }
