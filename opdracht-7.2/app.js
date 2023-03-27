a1253507764b011a6675438b92d2b276073311c5
jordyb010 <9015306@student.zadkine.nl>
2023-03-27 10:46:20 +0200

    klaar 7.2

---
+++ opdracht-7.2/app.js
@@ -0,0 +1,9 @@
+let output = document.getElementById(text);
+let input = document.getElementById(input);
+let shop = [];
+function add_product(){
+   let product = input.value; 
+   shop.push(product)
+    output.shop;
+    input = "";
+}
