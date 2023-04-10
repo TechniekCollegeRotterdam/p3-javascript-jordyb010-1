89f21693e7cd6aa560dede474caf425077247ae9
jordyb010 <9015306@student.zadkine.nl>
2023-04-10 19:51:19 +0200

    countdown klaar

---
+++ 7.3loop/app.js
@@ -0,0 +1,10 @@
+for (let i =0; i<= 100; i++){
+    console.log(i);
+}
+for (let i =100; i>= 0; i--){
+    console.log(i);
+}
+let shop = ["pruim", "apple" , "citroen"];
+for(let i = 0; i < shop.length; i++){
+    console.log(shop[i])
+}
